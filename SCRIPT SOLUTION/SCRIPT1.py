227785c46882f467c23d80328f67f6cd2ac1b658
22bca192 <127926304+22bca192@users.noreply.github.com>
2023-04-05 12:52:01 +0530

    Add files via upload

---
+++ SCRIPT SOLUTION/SCRIPT1.py
@@ -0,0 +1,11 @@
+#write a script enter the string check palidrome  or note.
+
+x=input("enter any string :")
+
+if(x==x):
+    print("this string is palidrome")
+else:
+    print("this string is not palidrome")
+    
+    
+
